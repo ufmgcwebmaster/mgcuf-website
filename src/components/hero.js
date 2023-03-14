d73288043f4e49190cc235a8a7774912e77845d9
Rithik Raja <rithik@Rithiks-MacBook-Pro.local>
2023-03-14 17:00:09 -0400

    eslint error

--- src/components/hero.js
+++ src/components/hero.js
@@ -15,7 +15,7 @@ export default function Hero() {
             <div className={`${styles["parallax-layer"]} ${styles["layer1"]} flex items-center justify-center`}>
                 <div className="w-1/2 h-1/2 bg-transparent flex flex-col items-center justify-center text-white text-center">
                     <h1 className={`text-2xl md:text-4xl lg:text-5xl xl:text-6xl mb-7 ${inter1.className}`}>The Multicultural Greek Council at the University of Florida</h1>
-                    <h2 className={`text-base md:text-lg xl:text-xl mb-3 ${roboto1.className}`}>"Unifying Leaders Through Diversity"</h2>
+                    <h2 className={`text-base md:text-lg xl:text-xl mb-3 ${roboto1.className}`}>{`"Unifying Leaders Through Diversity"`}</h2>
                     <h3 className={`text-sm md:text-base xl:text-lg ${roboto2.className}`}>Scholarship | Leadership | Service | Community</h3>
                 </div>
             </div>
